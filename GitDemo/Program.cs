3027f6ae235060d5b63c7ae666212b3ecbefe7df
Karishma <karishma.172k@gmail.com>
2021-11-16 17:39:28 +0530

    Add one line

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -8,6 +8,7 @@ namespace GitDemo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Git World");
+            Console.WriteLine("Today id Tuesday");
         }
     }
 }
